b40626eb76ad5f007241b83c30c31e248f62596e
Serhii Dankovych <serhiidankovych@gmail.com>
2026-01-28 17:16:20 +0200

    feat: add delay for global loading

--- src/store/loading-store.tsx
+++ src/store/loading-store.tsx
@@ -2,10 +2,23 @@ import { create } from "zustand";
 
 interface LoadingStore {
   isGlobalLoading: boolean;
-  setGlobalLoading: (loading: boolean) => void;
+  setGlobalLoading: (loading: boolean, minDuration?: number) => void;
 }
 
-export const useLoadingStore = create<LoadingStore>((set) => ({
+export const useLoadingStore = create<LoadingStore>((set, get) => ({
   isGlobalLoading: false,
-  setGlobalLoading: (loading: boolean) => set({ isGlobalLoading: loading }),
+
+  setGlobalLoading: (loading: boolean, minDuration: number = 0) => {
+    if (loading) {
+      set({ isGlobalLoading: true });
+    } else {
+      if (minDuration > 0) {
+        setTimeout(() => {
+          set({ isGlobalLoading: false });
+        }, minDuration);
+      } else {
+        set({ isGlobalLoading: false });
+      }
+    }
+  },
 }));
